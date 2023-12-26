10958919f351c8af2e0661988a5ec50636b1fd44
Santhosh Reddy Lotla <lsanthosh1999@gmail.com>
2023-12-26 14:20:09 +0530

    c

--- qr.detector.py
+++ qr.detector.py
@@ -1,34 +1,40 @@
-from picamera import PiCamera
-from time import sleep
 import cv2
+from time import sleep
 from pyzbar.pyzbar import decode
 
 def capture_qr_codes():
-    camera = PiCamera()
-    camera.resolution = (640, 480)
+    # Initialize the camera (if using the default camera index 0)
+    camera = cv2.VideoCapture(0)
 
     try:
         while True:
             sleep(2)  # Adjust delay between captures as needed
 
-            # Capture an image using the PiCamera
-            camera.capture('image.jpg')  # Save the image
+            # Capture a frame from the camera
+            ret, frame = camera.read()
 
-            # Read the saved image and decode QR codes
-            image = cv2.imread('image.jpg')
-            barcodes = decode(image)
+            # Check if the frame was successfully captured
+            if ret:
+                cv2.imwrite('image.jpg', frame)  # Save the captured frame as an image
 
-            if barcodes:
-                for barcode in barcodes:
-                    qr_data = barcode.data.decode('utf-8')
-                    print(f"QR Code Data: {qr_data}")
-                    # Perform actions based on the QR data here
+                # Read the saved image and decode QR codes
+                image = cv2.imread('image.jpg')
+                barcodes = decode(image)
 
+                if barcodes:
+                    for barcode in barcodes:
+                        qr_data = barcode.data.decode('utf-8')
+                        print(f"QR Code Data: {qr_data}")
+                        # Perform actions based on the QR data here
+
+                else:
+                    print("No QR code detected.")
             else:
-                print("No QR code detected.")
+                print("Failed to capture frame.")
 
     finally:
-        camera.close()
+        # Release the camera
+        camera.release()
 
 # Run the function to start capturing images and detecting QR codes
 capture_qr_codes()
