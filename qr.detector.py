90340951c0f04c916a8347f34a9f3f7927247ce9
Santhosh Reddy Lotla <lsanthosh1999@gmail.com>
2023-12-26 14:27:19 +0530

    t

--- qr.detector.py
+++ qr.detector.py
@@ -6,20 +6,23 @@ def capture_qr_codes():
     # Initialize the camera (if using the default camera index 0)
     camera = cv2.VideoCapture(0)
 
+    cv2.namedWindow("Camera Feed")  # Create a window to show the camera feed
+
+    frame_count = 0  # Variable to track captured frames
+
     try:
         while True:
-            sleep(2)  # Adjust delay between captures as needed
+            sleep(0.1)  # Adjust delay between captures as needed
 
             # Capture a frame from the camera
             ret, frame = camera.read()
 
             # Check if the frame was successfully captured
             if ret:
-                cv2.imwrite('image.jpg', frame)  # Save the captured frame as an image
+                cv2.imshow("Camera Feed", frame)  # Display the captured frame
 
                 # Read the saved image and decode QR codes
-                image = cv2.imread('image.jpg')
-                barcodes = decode(image)
+                barcodes = decode(frame)
 
                 if barcodes:
                     for barcode in barcodes:
@@ -27,14 +30,16 @@ def capture_qr_codes():
                         print(f"QR Code Data: {qr_data}")
                         # Perform actions based on the QR data here
 
-                else:
-                    print("No QR code detected.")
+                if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
+                    break
+
             else:
                 print("Failed to capture frame.")
 
     finally:
-        # Release the camera
+        # Release the camera and destroy OpenCV windows
         camera.release()
+        cv2.destroyAllWindows()
 
 # Run the function to start capturing images and detecting QR codes
 capture_qr_codes()
