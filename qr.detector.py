41782b25b0855e2f9b74328a7682d3c71bcd7f02
Santhosh Reddy Lotla <68048668+SanthoshReddyLotla@users.noreply.github.com>
2023-12-30 12:04:46 +0530

    Update qr.detector.py

--- qr.detector.py
+++ qr.detector.py
@@ -1,45 +1,42 @@
-import cv2
 from time import sleep
+from io import BytesIO
+import picamera
+from PIL import Image
 from pyzbar.pyzbar import decode
 
 def capture_qr_codes():
-    # Initialize the camera (if using the default camera index 0)
-    camera = cv2.VideoCapture(0)
+    with picamera.PiCamera() as camera:
+        camera.resolution = (640, 480)  # Set the resolution according to your requirements
+        sleep(2)  # Allow the camera to warm up
 
-    cv2.namedWindow("Camera Feed")  # Create a window to show the camera feed
+        try:
+            stream = BytesIO()
+            for _ in camera.capture_continuous(stream, format='jpeg'):
+                stream.seek(0)  # Reset stream position to the beginning
+                image = Image.open(stream)  # Open image using PIL
 
-    frame_count = 0  # Variable to track captured frames
+                # Convert PIL image to numpy array for decoding
+                frame = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-    try:
-        while True:
-            sleep(0.1)  # Adjust delay between captures as needed
-
-            # Capture a frame from the camera
-            ret, frame = camera.read()
-
-            # Check if the frame was successfully captured
-            if ret:
-                cv2.imshow("Camera Feed", frame)  # Display the captured frame
-
-                # Read the saved image and decode QR codes
+                # Decode QR codes
                 barcodes = decode(frame)
-#test
+
                 if barcodes:
                     for barcode in barcodes:
                         qr_data = barcode.data.decode('utf-8')
                         print(f"QR Code Data: {qr_data}")
                         # Perform actions based on the QR data here
 
-                if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to quit
-                    break
+                stream.seek(0)
+                stream.truncate()
 
-            else:
-                print("Failed to capture frame.")
+                # Adjust the delay between captures as needed
+                sleep(0.1)
 
-    finally:
-        # Release the camera and destroy OpenCV windows
-        camera.release()
-        cv2.destroyAllWindows()
+        except KeyboardInterrupt:
+            pass
+        finally:
+            camera.close()
 
 # Run the function to start capturing images and detecting QR codes
 capture_qr_codes()
